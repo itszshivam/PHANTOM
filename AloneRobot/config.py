1884a11c9d3503ebd36303519b5f3e618c0a9897
rocks143014 <119806738+rocks143014@users.noreply.github.com>
2023-09-19 10:18:56 +0530

    Update config.py

--- AloneRobot/config.py
+++ AloneRobot/config.py
@@ -14,17 +14,17 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = "14050586" # integer value, dont use ""
-    API_HASH = "42a60d9c657b106370c79bb0a8ac560c"
-    TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = "6079943111" # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "ALONE_WAS_BOT"
-    SUPPORT_CHAT = "AlonesHeaven"  # Your own group for support, do not add the @
+    API_ID = "25210670" # integer value, dont use ""
+    API_HASH = "5fcdf5f1a71872eb6af1a0af1681d571"
+    TOKEN = "6082718427:AAHCpXGuNKhH86IdGACze8U_DBign-yurhU"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = "6545714937" # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "ROCKY_ISS_BACK"
+    SUPPORT_CHAT = "XD_NETWORKS"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001603822916
+        -1001885729197
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001603822916
+        -1001885729197
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
     MONGO_DB_URI= "mongodb+srv://bsdk:betichod@cluster0.fgj1r9z.mongodb.net/?retryWrites=true&w=majority"
     # RECOMMENDED
@@ -34,20 +34,20 @@ class Config(object):
     WEBHOOK = False
     INFOPIC = True
     URL = None
-    SPAMWATCH_API = "ibGjchtz3djmcaIDiG3qoLX8KE0Npo1ChFiH7QGpjoQY1uB7BGOPJ~DOT07c0VSH"  # go to support.spamwat.ch to get key
+    SPAMWATCH_API = "it0XwldmTQVX27t7rJR17b8qCurFRwEjEUVXReJKd3rlfHWXnMFluPqMBDvH~tLy"  # go to support.spamwat.ch to get key
     SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
 
     # OPTIONAL
     ##List of id's -  (not usernames) for users which have sudo access to the bot.
-    DRAGONS = get_user_list("elevated_users.json", "6079943111")
+    DRAGONS = get_user_list("elevated_users.json", "6545714937")
     ##List of id's - (not usernames) for developers who will have the same perms as the owner
-    DEV_USERS = get_user_list("elevated_users.json", "6079943111")
+    DEV_USERS = get_user_list("elevated_users.json", "6545714937")
     ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    DEMONS = get_user_list("elevated_users.json", "6079943111")
+    DEMONS = get_user_list("elevated_users.json", "6545714937")
     # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
-    TIGERS = get_user_list("elevated_users.json", "6079943111")
+    TIGERS = get_user_list("elevated_users.json", "6545714937")
     WOLVES = get_user_list("elevated_users.json", "whitelists")
-    DONATION_LINK = "https://t.me/ALONE_WAS_BOT" # EG, paypal
+    DONATION_LINK = "https://t.me/ROCKY_ISS_BACK" # EG, paypal
     CERT_PATH = None
     PORT = 5000
     DEL_CMDS = True  # Delete commands that users dont have access to, like delete /ban if a non admin uses it.
